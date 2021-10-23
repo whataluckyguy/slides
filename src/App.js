f41508977cab6377c31465d98d23b0000972538e
Lalit Kumar <67325910+whataluckyguy@users.noreply.github.com>
2021-10-23 10:47:18 +0530

    Success Push

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="gradient"></div>
+      <img
+        src="https://res.cloudinary.com/dx6zgsncl/image/upload/v1634840053/Lucky/Carousel/one_beach_lzgzpm.jpg"
+        alt="travelquail"
+      />
+    </div>
+  );
+}
+
+export default App;
