f958af967079bba42bb8b0736d9070130d4ac7be
Lalit Kumar <67325910+whataluckyguy@users.noreply.github.com>
2021-10-24 23:27:18 +0530

    Fixed the ratio issue

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ function App() {
   return (
     <div className="App">
       <div className="gradient"></div>
-      <img
-        src="https://res.cloudinary.com/dx6zgsncl/image/upload/v1634840053/Lucky/Carousel/one_beach_lzgzpm.jpg"
-        alt="travelquail"
-      />
+      <div className="img">
+        <img
+          src="https://res.cloudinary.com/dx6zgsncl/image/upload/v1634840053/Lucky/Carousel/one_beach_lzgzpm.jpg"
+          alt="travelquail"
+        />
+      </div>
     </div>
   );
 }
